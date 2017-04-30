864b9a68bad7eb924002784c2ab1785d951e8da3
Tristan Seifert <t@tseifert.me>
2017-04-30 16:44:53 -0500

    add some mines

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -2,13 +2,60 @@
 
 #include "MainWindow.h"
 
+#include <stdexcept>
+
+using namespace std;
+
 /**
  * Initializes the game board.
  */
 GameBoard::GameBoard(int x, int y, int w, int h, MainWindow *parent) : Fl_Box(FL_DOWN_BOX, x, y, (w * CELL_SIZE), (h * CELL_SIZE), nullptr), _parent(parent) {
+	this->gridW = w;
+	this->gridH = h;
 
+	// create storage
+	this->storage.clear();
+
+	for(int x = 0; x < w; x++) {
+		this->storage.push_back(vector<TileType>());
+
+		for(int y = 0; y < h; y++) {
+			// cleared
+			this->storage[x].push_back(0);
+		}
+	}
 }
 
 GameBoard::~GameBoard() {
+
+}
+
+/**
+ * Randomly distributes mines in the board.
+ */
+void GameBoard::generateMines(int mines) {
+	if(mines > (this->gridW * this->gridH)) {
+		throw runtime_error("cannot fill more mines than there are cells");
+	}
+
+	int minesPlaced = 0;
+
+	while(minesPlaced != mines) {
+		// generate random X/Y coordinate
+		int x = arc4random_uniform(this->gridW);
+		int y = arc4random_uniform(this->gridH);
+
+		// is there already a mine here?
+		if(this->storage[x][y][MINE_BIT] != true) {
+			this->storage[x][y][MINE_BIT] = true;
+			minesPlaced++;
+		}
+	}
+}
+
+/**
+ * Updates the grid contents.
+ */
+void GameBoard::updateGrid() {
 
 }
--- GameBoard.h
+++ GameBoard.h
@@ -5,12 +5,24 @@
 #ifndef GAMEBOARD_H
 #define GAMEBOARD_H
 
-class MainWindow;
-
 #include <FL/Fl_Box.H>
+
+#include <vector>
+#include <bitset>
+
+class MainWindow;
 
 // cell size, square pixels
 static const int CELL_SIZE = 16;
+
+// is this cell a mine?
+static const int MINE_BIT = 0x0;
+// was this cell flagged?
+static const int FLAGGED_BIT = 0x1;
+// has this cell been uncovered?
+static const int UNCOVERED_BIT = 0xF;
+
+typedef std::bitset<16> TileType;
 
 class GameBoard : public Fl_Box {
 	public:
@@ -20,10 +32,22 @@ class GameBoard : public Fl_Box {
 		GameBoard(int x, int y, int w, int h, MainWindow *parent);
 		~GameBoard();
 
-	private:
+		void generateMines(int mines);
+
+	protected:
+		void updateGrid();
 
 	private:
 		MainWindow *_parent;
+
+		// size of the grid
+		int gridW, gridH;
+
+		// storage
+		std::vector<std::vector<TileType>> storage;
+
+		// when set, all mines are displayed
+		bool debugMode = true;
 };
 
 #endif
